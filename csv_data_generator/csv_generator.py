1510e6c01154cda1bfc846e38ee8956151ad7a3f
Bruno Fagundez <bfagundez@gmail.com>
2023-08-10 10:24:02 -0700

    Fixed array notation, added tests

--- csv_data_generator/csv_generator.py
+++ csv_data_generator/csv_generator.py
@@ -4,11 +4,16 @@ def default_generator(rows: int = 5, cols: int = 5) -> str:
     """Generate a csv file with default values."""
     output = ""
     for header in range(cols):
-        output += f"col{header},"
+        output += f"col{header+1}"
+        if header != range(cols)[-1]:
+            output += ","
     output += "\n"
     for x in range(rows):
         for y in range(cols):
-            output += f"{y},"
-        output += "\n"
+            output += f"{y+1}"
+            if y != range(cols)[-1]:
+                output += ","
+        if x != range(rows)[-1]:
+            output += "\n"
     return output
 
--- tests/test_csv_generator.py
+++ tests/test_csv_generator.py
@@ -8,4 +8,10 @@ sys.path.insert(0, PROJECT_DIR)
 from csv_data_generator.csv_generator import default_generator
 
 def test_basic_output():
-    assert default_generator() == "col0,col1,col2,col3,col4,\n0,1,2,3,4,\n0,1,2,3,4,\n0,1,2,3,4,\n0,1,2,3,4,\n0,1,2,3,4,\n"
+    assert default_generator() == "col1,col2,col3,col4,col5\n1,2,3,4,5\n1,2,3,4,5\n1,2,3,4,5\n1,2,3,4,5\n1,2,3,4,5"
+def test_provided_only_rowcount():
+    assert default_generator(10) == "col1,col2,col3,col4,col5\n1,2,3,4,5\n1,2,3,4,5\n1,2,3,4,5\n1,2,3,4,5\n1,2,3,4,5\n1,2,3,4,5\n1,2,3,4,5\n1,2,3,4,5\n1,2,3,4,5\n1,2,3,4,5"
+def test_provided_only_colcount():
+    assert default_generator(cols=3) == "col1,col2,col3\n1,2,3\n1,2,3\n1,2,3\n1,2,3\n1,2,3"
+def test_provided_colcount():
+    assert default_generator(3,10) == "col1,col2,col3,col4,col5,col6,col7,col8,col9,col10\n1,2,3,4,5,6,7,8,9,10\n1,2,3,4,5,6,7,8,9,10\n1,2,3,4,5,6,7,8,9,10"
