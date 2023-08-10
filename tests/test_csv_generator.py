eeb0ac08c35bf15abc5b2c8b2c7438e0c48f44e5
Bruno Fagundez <bfagundez@gmail.com>
2023-08-10 14:20:32 -0700

    Better formatting on tests

--- tests/test_csv_generator.py
+++ tests/test_csv_generator.py
@@ -9,22 +9,37 @@ from csv_data_generator.csv_generator import default_generator
 
 
 def test_basic_output():
-    assert default_generator(
-    ) == "col1,col2,col3,col4,col5\n1,2,3,4,5\n1,2,3,4,5\n1,2,3,4,5\n1,2,3,4,5\n1,2,3,4,5"
-
+    assert default_generator() == ''.join(("col1,col2,col3,col4,col5\n",
+                                          "1,2,3,4,5\n",
+                                          "1,2,3,4,5\n", 
+                                          "1,2,3,4,5\n", 
+                                          "1,2,3,4,5\n", 
+                                          "1,2,3,4,5"))
 
 def test_provided_only_rowcount():
-    assert default_generator(
-        10
-    ) == "col1,col2,col3,col4,col5\n1,2,3,4,5\n1,2,3,4,5\n1,2,3,4,5\n1,2,3,4,5\n1,2,3,4,5\n1,2,3,4,5\n1,2,3,4,5\n1,2,3,4,5\n1,2,3,4,5\n1,2,3,4,5"
-
+    assert default_generator(10) == ''.join(("col1,col2,col3,col4,col5\n",
+                                          "1,2,3,4,5\n",
+                                          "1,2,3,4,5\n",
+                                          "1,2,3,4,5\n",
+                                          "1,2,3,4,5\n",
+                                          "1,2,3,4,5\n",
+                                          "1,2,3,4,5\n",
+                                          "1,2,3,4,5\n", 
+                                          "1,2,3,4,5\n", 
+                                          "1,2,3,4,5\n", 
+                                          "1,2,3,4,5"))
 
 def test_provided_only_colcount():
-    assert default_generator(
-        cols=3) == "col1,col2,col3\n1,2,3\n1,2,3\n1,2,3\n1,2,3\n1,2,3"
-
+    assert default_generator(cols=3) == ''.join(("col1,col2,col3\n",
+                                          "1,2,3\n",
+                                          "1,2,3\n",
+                                          "1,2,3\n",
+                                          "1,2,3\n",
+                                          "1,2,3"))
 
 def test_provided_colcount():
-    assert default_generator(
-        3, 10
-    ) == "col1,col2,col3,col4,col5,col6,col7,col8,col9,col10\n1,2,3,4,5,6,7,8,9,10\n1,2,3,4,5,6,7,8,9,10\n1,2,3,4,5,6,7,8,9,10"
+    assert default_generator(3, 10) == ''.join(("col1,col2,col3,col4,col5,col6,col7,col8,col9,col10\n",
+                                          "1,2,3,4,5,6,7,8,9,10\n",
+                                          "1,2,3,4,5,6,7,8,9,10\n",
+                                          "1,2,3,4,5,6,7,8,9,10"))
+
