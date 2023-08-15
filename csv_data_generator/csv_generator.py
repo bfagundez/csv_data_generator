1e597bcc490ae8fd082cdefe932802ab387564b6
Bruno Fagundez <bfagundez@gmail.com>
2023-08-15 08:32:24 -0700

    refactored the generator a bit more

--- csv_data_generator/csv_generator.py
+++ csv_data_generator/csv_generator.py
@@ -5,17 +5,24 @@ class CSVGenerator:
 
     def generate(self) -> str:
         """Generate a csv file with default values."""
+        return self.headerGenerator() + self.rowsGenerator()
+   
+    def headerGenerator(self) -> str:
         output = ""
         for header in range(self.cols):
             output += f"col{header+1}"
             if header != range(self.cols)[-1]:
                 output += ","
         output += "\n"
+        return output
+    
+    def rowsGenerator(self) -> str:
+        """Generate rows with default values."""
+        output = ""
         for x in range(self.rows):
             lastRow = x != range(self.rows)[-1]
             output += self.rowGenerator(lastRow)
         return output
-
 
     def rowGenerator(self,lastRow = False) -> str:
         """Generate a row with default values."""
