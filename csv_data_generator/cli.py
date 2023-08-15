005f504a3cdc7cee53d7ca9e7adf1339490a0d72
Bruno Fagundez <bfagundez@gmail.com>
2023-08-15 08:36:11 -0700

    fixed cli import

--- csv_data_generator/cli.py
+++ csv_data_generator/cli.py
@@ -1,9 +1,10 @@
 import click
-from .csv_generator import default_generator
+from .csv_generator import CSVGenerator
 
 
 @click.command()
 @click.option('--rows', default=5, help='Number of rows.')
 @click.option('--cols', default=5, help='Number of cols.')
 def start(rows, cols):
-    click.echo(default_generator(rows, cols))
+    csv_generator = CSVGenerator(rows, cols)
+    click.echo(csv_generator.generate())
